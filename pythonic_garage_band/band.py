79f923ba10be53b9ca4adf9632fd2f1da67b2aa2
evidad <errolvidad@gmail.com>
2024-01-11 13:51:14 -0800

    passed test 4

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -23,3 +23,6 @@ class Drummer:
 
     def __str__(self):
         return f"My name is {self.name} and I play drums"
+    
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
